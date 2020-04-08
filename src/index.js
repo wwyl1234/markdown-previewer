a62da4c5c30d6194b7d3e67fa4af5ff390bf1ab5
Winnie Lam <winnielam@Winnies-MacBook-Air.local>
2020-04-08 11:35:33 -0400

    Created the markdown previwer with minimal style.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import marked from 'marked';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class MarkdownPreviwerApp extends React.Component {
+    render(){
+        return (
+            <div id='markdown-preview-app'>
+                <MarkdownPreviwer />
+            </div>
+        )
+    }
+}
+
+class MarkdownPreviwer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            input: '',
+            output: {__html: marked('', {sanitize: true})}
+        }
+        this.handleOnChange = this.handleOnChange.bind(this);
+
+    }
+    handleOnChange = (event) => {
+        let newInput = event.target.value
+        this.setState({
+            input: newInput,
+            output: {__html: marked(newInput, {sanitize: true})}
+        })
+    }
+
+    render(){
+        return(
+            <div is='markdown-preview'>
+                <div id='editor-container'>
+                    <h1>Editor</h1>
+                    <textarea id='editor' placeholder='Enter markdown here:' onChange={this.handleOnChange}></textarea>
+                </div>
+                <div id='preview-container'>
+                    <h1>Preview</h1>
+                    <div id='preview' dangerouslySetInnerHTML={this.state.output}></div>
+                </div>
+            </div>
+        )
+    }
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <MarkdownPreviwerApp />,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
